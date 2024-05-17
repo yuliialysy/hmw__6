6b7bdb0af9d136b9584eed96039e4557fe412e70
yuliialysy <164245481+yuliialysy@users.noreply.github.com>
2024-05-17 21:06:29 +0100

    Update bott.py

--- bott.py
+++ bott.py
@@ -15,6 +15,7 @@ class Name(Field):
 
 class Phone(Field):
     def __init__(self, value):
+            
         super().__init__(value)
     
     def validate(self):
@@ -28,16 +29,18 @@ class Record:
         self.name = Name(name)
         self.phones = []
 
+
     def add_phone(self, phone):
         self.phones.append(Phone(phone))
 
     def remove_contact(self, phone):
         self.phones = [p for p in self.phones if p.value != phone]
+    
 
     def edit_phone(self, old_phone, new_phone):
         for phone in self.phones:
             if phone.value == old_phone:
-                phone.value == new_phone
+                phone.value = new_phone
                 break
     
     def find_phone(self, phone):
